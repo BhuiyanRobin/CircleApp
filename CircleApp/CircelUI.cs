862fdf84184647e7ad828d6fce1037af8da77015
bhuiyanrobin <bhuiyanrobin93@live.com>
2014-12-26 13:59:37 +0600

    sdfdsf
    Signed-off-by: bhuiyanrobin <bhuiyanrobin93@live.com>

---
+++ CircleApp/CircelUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircleApp
+{
+    public partial class CircelUI : Form
+    {
+        public CircelUI()
+        {
+            InitializeComponent();
+        }
+        Circle aCircle=new Circle();
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            aCircle.radious = Convert.ToDouble(radiusTextBox.Text);
+            diameterTextBox.Text = aCircle.GetDiameter().ToString();
+            perimeterTextBox.Text = aCircle.GetPeremeter().ToString();
+            areaTextBox.Text = aCircle.GetArea().ToString();
+        }
+    }
+}
---
+++ CircleApp/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Circle
+    {
+        public double radious;
+
+        public double GetArea()
+        {
+            return 3.1416*radious*radious;
+        }
+        public double GetPeremeter()
+        {
+            return 2 * 3.1416 * radious;
+
+        }
+        public double GetDiameter()
+        {
+            return 2*radious;
+        }
+        
+
+
+    }
+
+}
